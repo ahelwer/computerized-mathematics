22922519febf2b115bd833124f7b9ced1752b62a
Neil Penning <neil.penning@gmail.com>
2021-07-16 16:38:24 -0400

    changed append to write; updated strat file

--- card_game/4group/makestrats.py
+++ card_game/4group/makestrats.py
@@ -1,14 +1,14 @@
 exec(open("cards4.py").read())
 
 
-with open('strat_p1.out.txt', 'a') as strat1:
+with open('strat_p1.out.txt', 'w') as strat1:
     for a in S:
         for b in S:
             for c in S:
                 strat1.write(f"(x, {a}, {b}, {c}) -> {m.evaluate(t1(a, b, c))}\n")
 
 
-with open('strat_p2.out.txt', 'a') as strat2:
+with open('strat_p2.out.txt', 'w') as strat2:
     for a in S:
         for b in S:
             for c in S:
@@ -16,14 +16,14 @@ with open('strat_p2.out.txt', 'a') as strat2:
 
 
 
-with open('strat_p3.out.txt', 'a') as strat3:
+with open('strat_p3.out.txt', 'w') as strat3:
     for a in S:
         for b in S:
             for c in S:
                 strat3.write(f"({a}, {b}, x, {c}) -> {m.evaluate(t3(a, b, c))}\n")
 
 
-with open('strat_p4.out.txt', 'a') as strat4:
+with open('strat_p4.out.txt', 'w') as strat4:
     for a in S:
         for b in S:
             for c in S:
